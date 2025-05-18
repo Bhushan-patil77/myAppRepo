df83ac5a160b3592bcd8dad4a96f8b13b9f328ec
Bhushan-patil77 <145292155+Bhushan-patil77@users.noreply.github.com>
2025-05-18 15:43:22 +0530

    Update GetMessageController.java

--- src/main/java/com/example/demo/controller/GetMessageController.java
+++ src/main/java/com/example/demo/controller/GetMessageController.java
@@ -8,6 +8,6 @@ public class GetMessageController {
 
 	@GetMapping("/getMessage")
 	public String getMessage() {
-		return "Hello world once again this is cicd testing full flow   hellooooooooo";
+		return "Hii bhushan...";
 	}
 }
